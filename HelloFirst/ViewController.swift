0baabccc3e9e47ff6bcd9305a454fd1acd5ad430
mincheol <homerjay@naver.com>
2021-11-05 00:30:19 +0900

    Initial Commit

---
+++ HelloFirst/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloFirst
+//
+//  Created by 申民鐡 on 2021/11/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
