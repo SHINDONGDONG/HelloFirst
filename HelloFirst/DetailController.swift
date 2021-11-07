34337b80fdf8bb9a5d362b3bf1f7e9d9565a63c4
mincheol <homerjay@naver.com>
2021-11-08 06:46:04 +0900

    webkit 언랩핑

--- HelloFirst/DetailController.swift
+++ HelloFirst/DetailController.swift
@@ -6,10 +6,25 @@
 //
 
 import UIKit
+import WebKit
+
 
 class DetailController: UIViewController {
+    @IBOutlet weak var WebViewMain: WKWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //1. web을 키는방법 url string
+        //2. url > request
+        //3. req > webview load
+
+        let urlString = "https:www.google.com"
+        if let url = URL(string: urlString){ //unwrap 스트링을 url으로 바꾸어주는 형태 옵셔널 바인딩
+            let urlReq = URLRequest(url: url)
+            WebViewMain.load(urlReq)
+        }
+        
     }
     
     
