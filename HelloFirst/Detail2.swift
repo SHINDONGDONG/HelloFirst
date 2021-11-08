aa25f2f44147cbd1a8a94501ac3aaca1b3e62c83
mincheol <homerjay@naver.com>
2021-11-09 06:53:42 +0900

    table view, as, click event

--- HelloFirst/Detail2.swift
+++ HelloFirst/Detail2.swift
@@ -20,10 +20,20 @@ class Detail2 :UIViewController,UITableViewDelegate,UITableViewDataSource{
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
      //데이터 무엇?  반복할거가타...
     // 임의의 셀을 만들기
-        let cell = UITableViewCell.init(style: .default, reuseIdentifier: "Table Cell 1")
-        cell.textLabel?.text = "\(indexPath.row+1)번으로 출력이 됩니다."
+//        let cell = UITableViewCell.init(style: .default, reuseIdentifier: "Table Cell 1")
+//        cell.textLabel?.text = "\(indexPath.row+1)번으로 출력이 됩니다."
         
+        //2번방법
+        //다시 재생할 수 있는 셀을 정의해서  테이블뷰메인에 꽂아넣는다.
+        let cell = TableViewMain.dequeueReusableCell(withIdentifier: "Type1", for: indexPath) as! Type1
+        //as? as! 부모자식 친자확인
+        cell.LabelText.text = "\(indexPath.row+1)번으로 출력이 됩니다."
         return cell
+    }
+    
+    //클릭
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("Click!! \(indexPath.row)")
     }
     
     override func viewDidLoad() {
---
+++ HelloFirst/Type1.swift
@@ -0,0 +1,13 @@
+//
+//  Type1.swift
+//  HelloFirst
+//
+//  Created by 申民鐡 on 2021/11/09.
+//
+
+import UIKit
+
+class Type1 : UITableViewCell {
+    
+    @IBOutlet weak var LabelText: UILabel!
+}
